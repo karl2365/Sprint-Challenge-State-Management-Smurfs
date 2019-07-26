6d0748c92dcb761ac46fbe640acd49b6403fae3a
karl2365 <karlbeksa@gmail.com>
2019-07-26 12:48:59 -0400

    MVP Complete - no styling

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -13,45 +13,13 @@ class Smurf extends React.Component  {
     }
 
     
-    handleChanges = e => {
-        this.setState ({
-            ...this.state, 
-            [e.target.name]: e.target.value
-        })
-    }
 
-    handleSubmit = e => {
-        e.preventDefault();
-        console.log(this.state)
-            Axios
-            .post('http://localhost:3333/smurfs', this.state)
-            .then(res => {
-                console.log(res);
-                this.props.setSmurfs(res.data);
-
-              
-            })
-            .catch(err => console.log(err.response))
-      
-    }
 
     render (){
         return (
             <div className='smurfContainer'>
                 {this.props.smurf.name}
-                <h2 className="formTitle">Add Smurf</h2>
-                <form className='form' onSubmit={this.handleSubmit}>
-                
-                <label>Name: </label>
-                    <input type='text' name='name' onChange={this.handleChanges} value={this.state.name}  />
-                
-                <label>Age: </label>
-                    <input type='number' name='age' onChange={this.handleChanges} value={this.state.age}  />    
-                
-                <label>Height: </label>
-                    <input type='text' name='height' onChange={this.handleChanges} value={this.state.height}  />
-                <button type='submit'>Add Smurf</button>
-                </form>
+  
             </div>
         )
     }    
--- smurfs/src/components/Smurfs.js
+++ smurfs/src/components/Smurfs.js
@@ -1,16 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import Axios from 'axios';
 
 import { SmurfContext } from '../contexts/SmurfContext';
 import Smurf from './Smurf';
 
 const Smurfs = () => {
     const { smurfs, setSmurfs } = useContext(SmurfContext);
+    const [newSmurf, setNewSmurf] = useState({name: '', age: 0, height: ''});
 
 
+    const handleChanges = e => {
+        setNewSmurf ({
+            ...newSmurf, 
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        //e.preventDefault();
+        console.log(newSmurf)
+     
+            Axios
+            .post('http://localhost:3333/smurfs', newSmurf)
+            .then(res => {
+                console.log(res);
+                this.props.setSmurfs(res.data);
+                window.location.reload();
+              
+            })
+            .catch(err => console.log(err.response))
+  
+    }
 
     return (
         <div>
             {smurfs.map(smurf => <Smurf smurf={smurf} key={smurf.id} setSmurfs={setSmurfs} />)}
+            <h2 className="formTitle">Add Smurf</h2>
+                <form className='form' onSubmit={handleSubmit}>
+                
+                <label>Name: </label>
+                    <input type='text' name='name' onChange={handleChanges} value={newSmurf.name}  />
+                
+                <label>Age: </label>
+                    <input type='number' name='age' onChange={handleChanges} value={newSmurf.age}  />    
+                
+                <label>Height: </label>
+                    <input type='text' name='height' onChange={handleChanges} value={newSmurf.height}  />
+                <button type='submit'>Add Smurf</button>
+                </form>
         </div>
     )
 
